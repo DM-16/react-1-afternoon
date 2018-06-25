e452eea752550b65a51c49bcf009ff81d38db17f
jdaniel <jdricher2017@gmail.com>
2018-06-25 16:19:22 -0500

    finished

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 
 export default class FilterObject extends Component {
-
     constructor() {
         super();
-
         this.state = {
             employees: [
                 {
