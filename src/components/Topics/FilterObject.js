dab3fcddf0fdcd1a46dbb80b58a5ede17abd9233
jdaniel <jdricher2017@gmail.com>
2018-07-07 12:16:14 -0500

    completed

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,52 +1,50 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class FilterObject extends Component {
-    constructor() {
-        super();
-        this.state = {
-            employees: [
-                {
-                    name: 'Jimmy Joe',
-                    title: 'Hack0r',
-                    age: 12,
-                },
-                {
-                    name: 'Jeremy Schrader',
-                    age: 24,
-                    hairColor: 'brown'
-                },
-                {
-                    name: 'Carly Armstrong',
-                    title: 'CEO',
-                }
-            ],
-            userInput: '',
-            filteredEmployees: []
-        }
-    }
-    handleChange(value) {
-        this.setState({ userInput: value });
-    }
-    filteredEmployees(prop) {
-        let employees = this.state.employees;
-        let filteredEmployees = [];
+  constructor(props) {
+    super(props);
+    this.state = {
+      unFilteredArray: [
+        { name: "Jimmy Joe", title: "Hack0r", age: 12 },
+        { name: "Jeremy Schrader", age: 24, hairColor: "brown" },
+        { name: "Carly Armstrong", title: "CEO" }
+      ],
+      unFilteredArrayString:
+        '[ { "name": "Jimmy Joe", "title": "Hack0r", "age": 12 }, { "name": "Jeremy Schrader", "age": 24, "hairColor": "brown" }, { "name": "Carly Armstrong", "title": "CEO" } ]',
+      userInput: "",
+      filteredArray: []
+    };
+  }
 
-        for (let i = 0; i < employees.length; i++) {
-            if (employees[i].hasOwnProperty(prop)) {
-                filteredEmployees.push(employees[i]);
-            }
-        }
-        this.setState({ filteredEmployees: filteredEmployees });
-    }
-    render() {
-        return (
-            <div className="puzzleBox filterObjectPB">
-                <h4> Filter Object </h4>
-                <div className="puzzleText"> Original: {JSON.stringify(this.state.employees, null, 10)}</div>
-                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}></input>
-                <button className="confirmationButton" onClick={() => this.filteredEmployees(this.userInput)}>Filter </button>
-                <div className="resultsBox filterObjectRB"> Filtered: {JSON.stringify(this.state.filteredEmployees, null, 10)}</div>
-            </div>
-        )
-    }
+  handleInput(e) {
+    this.setState({ userInput: e.target.value });
+  }
+
+  handleClick(e) {
+    let arr = this.state.unFilteredArray.filter(element => {
+      return element.hasOwnProperty(this.state.userInput);
+    });
+    this.setState({ filteredArray: arr });
+  }
+
+  render() {
+    return (
+      <div className="puzzleBox filterObjectPB">
+        <h4>Filter Object</h4>
+        <span className="puzzleText">
+          Original:{JSON.stringify(this.state.unFilteredArray)}
+        </span>
+        <input className="inputLine" onChange={e => this.handleInput(e)} />
+        <button
+          className="confirmationButton"
+          onClick={e => this.handleClick(e)}
+        >
+          Submit
+        </button>
+        <span className="resultsBox filterObjectRB">
+          {JSON.stringify(this.state.filteredArray)}
+        </span>
+      </div>
+    );
+  }
 }
